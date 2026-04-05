c5b2c3d92129fa40a41b9d945c80d76f984adfcc
Thomas Boby <th.boby@gmail.com>
2026-04-05 19:41:44 +0000

    Optimise

--- ml2mqtt/classifiers/RandomForest.py
+++ ml2mqtt/classifiers/RandomForest.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder, OrdinalEncoder
-from sklearn.ensemble import RandomForestClassifier
+from sklearnex.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split, GridSearchCV, RandomizedSearchCV
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from sklearn.pipeline import Pipeline
